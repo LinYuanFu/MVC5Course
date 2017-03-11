80c0a26bbe4a18e73346755da8e84a796969be35
Austin <sheepfu@gmail.com>
2017-03-11 14:11:30 +0800

    05 練習表單欄位驗證，套用 Required 與 Range 驗證屬性，並自訂錯誤訊息

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,9 +22,14 @@ namespace MVC5Course.Models
         }
     
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "請輸入產品名稱 {0}")]
         public string ProductName { get; set; }
+        [Required]
+        [Range(10, 99999, ErrorMessage = "請輸入範圍介於 10 - 99999 之間的數值")]
         public Nullable<decimal> Price { get; set; }
+        [Required]
         public Nullable<bool> Active { get; set; }
+        [Required]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
